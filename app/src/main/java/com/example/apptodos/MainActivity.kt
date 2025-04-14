b3d97c006ecb637ec4dd6e89026302a0601cf32f
AlCaMiC <fadilandi57@gmail.com>
2025-04-14 13:45:27 +0800

    add design (main menu, add Task menu etc)

--- app/src/main/java/com/example/apptodos/MainActivity.kt
+++ app/src/main/java/com/example/apptodos/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.apptodos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import android.view.View
+import android.widget.TextView
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val tvDate = findViewById<TextView>(R.id.tvDate)
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault())
+        val formattedDate = dateFormat.format(calendar.time)
+        tvDate.text = formattedDate
+
+        tvDate.text = formattedDate
+    }
+    fun addTask (view: View){
+        val intent = Intent(this, AddTask::class.java)
+        startActivity(intent)
     }
 }
